8561b420d69a9cf4fe1b466155918473faaa1203
Martin Peshovski <peshovskiimartin@gmail.com>
2026-05-07 23:55:40 +0200

    login form components initial commit

---
+++ src/clients/spcex-web/src/app/auth/components/auth-form/auth-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-form',
+  imports: [],
+  templateUrl: './auth-form.html',
+  styleUrl: './auth-form.scss',
+})
+export class AuthForm {}
